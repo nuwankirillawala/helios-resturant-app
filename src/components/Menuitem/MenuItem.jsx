d82a29b136574f007e9d5047af71771fdc66665d
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-03-31 19:03:26 +0530

    Add a munu item component

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-
 import './MenuItem.scss';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({title, price, tags}) => (
+  <div className="app__menuItem">
+    <div className="app__menuItem-header">
+      <div className="app__menuItem-name">
+        <p className="p__cormorant" style={{color: '#DCCA87'}}>{title}</p>
+      </div>
+
+      <div className="app__menuItem-dash" />
+      <div className="app__menuItem-name">
+        <p className="p__cormorant">{price}</p>
+      </div>
+    </div>
+
+    <div className="app__menuItem-sub">
+      <p className="p__opensans" style={{color: '#AAA'}}>{tags}</p>
+    </div>
   </div>
 );
 
