df8d577a88f30a0fc9fd722c6894c104f4de80ae
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-03-31 16:52:24 +0530

    modify p color

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavor" />
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
-      <p className="p__opensans" style={{margin: '2rem 0'}}>
+      <p className="app__header-p p__opensans">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ea debitis labore distinctio esse quos magni suscipit tempore aperiam reiciendis ab ducimus velit dolor laudantium, officiis dolores iure ipsa illum.
       </p>
       <button className="custom__button">Explore Menu</button>
