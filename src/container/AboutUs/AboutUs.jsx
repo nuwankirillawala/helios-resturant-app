ce1fca78efdf88c3d604077ee65c3edcc54c5fcb
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-04-02 13:03:39 +0530

    update webp images

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,8 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import { images } from '../../constants';
 import './AboutUs.scss';
 
-const AboutUs = () => (
-  <div className="app__aboutus app__bg flex__center section__padding" id='about'>
+const AboutUs = () => {
+  const navigate = useNavigate();
+
+  const handleClick = ()=> {
+    navigate('/about');
+  }
+  return (
+    <div className="app__aboutus app__bg flex__center section__padding" id='about'>
     <div className="app__aboutus-overlay flex__center">
       <img src={images.H} alt="H Letter" />
     </div>
@@ -14,7 +21,7 @@ const AboutUs = () => (
         <p className="p__opensans">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate corrupti ratione suscipit. Debitis ullam adipisci dignissimos sit eum esse corrupti!
         </p>
-        <button type='button' className='custom__button'>See more</button>
+        <button type='button' className='custom__button' onClick={handleClick}>See more</button>
       </div>
 
       <div className="app__aboutus-content_knife flex__center">
@@ -31,6 +38,7 @@ const AboutUs = () => (
       </div>
     </div>
   </div>
-);
+  );
+}
 
 export default AboutUs;
