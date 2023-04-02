dbf13e58253e07f667b89473392b697a6402bd0f
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2023-04-02 12:47:17 +0530

    link sections

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,7 +15,8 @@ const Navbar = () => {
       <li className="p__opensans"><a href="#home">Home</a></li>
       <li className="p__opensans"><a href="#about">About</a></li>
       <li className="p__opensans"><a href="#menu">Menu</a></li>
-      <li className="p__opensans"><a href="#promo">Promo</a></li>
+      <li className="p__opensans"><a href="#gallery">Gallery</a></li>
+      <li className="p__opensans"><a href="#awards">Awards</a></li>
       <li className="p__opensans"><a href="#contact">Contact</a></li>
     </ul>
     <div className="app__navbar-login">
